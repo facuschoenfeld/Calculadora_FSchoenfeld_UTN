0859a514328f2756d9060d3bdc5871730ac3c87a
facuschoenfeld <facuschoenfeld@gmail.com>
2025-08-05 13:06:34 -0300

    calculadora v1.1

---
+++ calculadora_clase.mjs
@@ -0,0 +1,25 @@
+export class Calculadora{
+    sumar(a, b){
+        return a + b
+    }
+
+    restar(a, b){
+        return a - b
+    }
+
+    multiplicar(a, b){
+        return a * b
+    }
+
+    dividir(a, b){
+        return a / b
+    }
+
+    resto(a, b){
+        return a % b
+    }
+
+    potencia(a, b){
+        return Math.pow(a, b)
+    }
+}
--- index.mjs
+++ index.mjs
@@ -1,2 +1,24 @@
 import { input } from "./utils.mjs"
 import { Calculadora } from "./calculadora_clase.mjs"
+
+const calc = new Calculadora()
+
+while(true){
+    console.clear()
+    console.log(`
+        --- CALCULADORA ---
+        1. Sumar
+        2. Restar
+        3. Multiplicar
+        4. Dividir
+        5. Resto
+        6. Potencia
+        0. Salir
+        `)
+    const opcion = await input(":")
+
+    if(opcion === "0"){
+        console.log("Hasta luego...")
+        break
+    }
+}
